1ba2e3d3c9cfc8a70be635b95a78d8c36409001d
charis <christopinem01@gmail.com>
2022-06-24 16:00:32 +0700

    project pertama

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+ini adalah halaman contact....
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sapiente quae facilis laboriosam dolores cum nihil fugiat assumenda illum perferendis impedit aspernatur culpa, itaque ipsum reprehenderit asperiores quas maiores voluptatum?
+    Aut molestiae accusantium quia doloribus unde debitis quo aliquid reprehenderit ullam laboriosam provident est animi veritatis aliquam vel repellendus, dolorum dolores earum nostrum veniam nobis esse. Recusandae ex distinctio provident.
+    Quaerat perferendis id omnis expedita dolorum laborum, totam quo, culpa fuga quia, vero cupiditate explicabo delectus reprehenderit harum sequi sed dicta nisi. Nulla eaque molestias ducimus ex laborum enim consequuntur.
+    Iusto adipisci fuga similique, officiis voluptatum soluta numquam molestias suscipit tempore ipsa explicabo consectetur. Quas assumenda explicabo, quisquam dolor alias cupiditate illo deleniti modi rerum quae perspiciatis doloribus omnis quod!
+    Obcaecati porro eligendi numquam illo et nemo voluptatibus nobis, dolore iure voluptate nulla nostrum omnis, voluptatum nihil suscipit. Repudiandae eaque nam autem! Ipsa doloremque recusandae expedita excepturi doloribus modi pariatur!
+</p>
